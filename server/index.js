305b0546aa9ed69cb80771a127660a1187f95f1a
Kuldeep Singh <rathorekuldeeprajendrasingh932@gmail.com>
2025-08-07 12:44:15 +0530

    update: the setup for server

--- server/index.js
+++ server/index.js
@@ -1,9 +1,24 @@
-import connectDB from "./database/db.js";
-import app from "./app.js";
 import dotenv from "dotenv";
+import express from "express";
+import connectDB from "./config/connectdb.js";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
+const app = express();
 
+// middleware
+app.use(express.json()); // parses incoming JSON data
+app.use(cookieParser()); // read cookies in requests
+app.use(
+  // enables frontend-backend communication with cookies
+  cors({
+    origin: process.env.FRONTEND_URL, // frontend url
+    credentials: true, // allow cookies
+  })
+);
+
+// connect to database
 connectDB()
   .then(() => {
     app.listen(process.env.PORT || 5000, () => {
@@ -13,3 +28,7 @@ connectDB()
   .catch((error) => {
     console.log("Error Connecting to MongoDB:", error);
   });
+
+app.get("/", (req, res) => {
+  res.end("Hello world");
+});
