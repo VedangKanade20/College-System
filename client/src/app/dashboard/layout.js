0a45c14e660ebc9369cb153dc4a2ee9e8694efc7
Kuldeep Singh <rathorekuldeeprajendrasingh932@gmail.com>
2025-08-07 16:30:18 +0530

    update: fixed the position of sidebar

--- client/src/app/dashboard/layout.js
+++ client/src/app/dashboard/layout.js
@@ -3,9 +3,9 @@ import Sidebar from "./_components/Sidebar";
 
 export default function RootLayout({ children }) {
   return (
-    <section className="flex w-[100%]">
+    <section className="flex">
       <Sidebar />
-      <main className="flex-1 p-6 w-[80%] bg-[#F9FAFC] min-h-screen">
+      <main className="flex-1 p-6 ml-[20%] w-[80%] bg-[#F9FAFC] min-h-screen">
         <ProfileSegment />
         {children}
       </main>
