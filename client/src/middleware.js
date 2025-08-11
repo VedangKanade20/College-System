ff9f7f1b566d076328072d9a61f1baacb07b37d8
Kuldeep Singh <rathorekuldeeprajendrasingh932@gmail.com>
2025-08-11 20:47:22 +0530

    feat(auth):protect the login and sign-up page from loggedin user

--- client/src/middleware.js
+++ client/src/middleware.js
@@ -2,14 +2,23 @@ import { NextResponse } from "next/server";
 
 export function middleware(req) {
   const token = req.cookies.get("token")?.value;
+  const { pathname } = req.nextUrl;
 
-  if (!token && req.nextUrl.pathname.startsWith("/dashboard")) {
+  // If the user is not authenticated and trying to access dashboard routes, redirect to login
+  if (!token && pathname.startsWith("/dashboard")) {
     return NextResponse.redirect(new URL("/login", req.url));
+  }
+
+  if (
+    (token && pathname.startsWith("/login")) ||
+    pathname.startsWith("/signup")
+  ) {
+    return NextResponse.redirect(new URL("/dashboard", req.url));
   }
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/dashboard/:path*"], // protect dashboard routes
+  matcher: ["/dashboard/:path*", "/login", "/signup"], // protect dashboard routes
 };
